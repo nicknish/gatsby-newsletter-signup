901907602c56eb8b06943f642ccc45c12e7f0d77
nicknish <nickjnish@gmail.com>
2019-09-15 22:48:38 -0700

    init

---
+++ src/components/Signup.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react"
+
+const Signup = () => {
+  const [submitted, updateSubmitted] = useState(false)
+  const [error, updateError] = useState("")
+  const [email, updateEmail] = useState("")
+
+  const handleSubmit = () => {
+    /* TODO: Submit email to Mailchimp */
+    /* If error, then update error and display it */
+    updateEmail("")
+    updateSubmitted(true)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>Sign up for my newsletter</p>
+      <div>
+        <label>Email</label>
+      </div>
+      <input type="email" value={email} onChange={updateEmail} />
+
+      {submitted && <p>You're in the know!</p>}
+      {error && !submitted(<p>Please enter a valid email.</p>)}
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+export default Signup
